8222654459aed8608bc65ba24acf26bc6b361be5
Koen van der Linden <vanderlinden.koen@gmail.com>
2017-04-02 15:52:07 +0200

    Step 1 code of challenge

---
+++ CoolBreeze/CoolBreeze/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CoolBreeze
+{
+    public partial class App : Application
+    {
+        public static MainViewModel ViewModel;
+        public static string RegistrationCode = "XBWW904F9C7291A36B1F063E0F29D311";
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CoolBreeze.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
